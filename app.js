72ff7ebff5c816ebcb1392fb21d31bebb48efae8
Byungchul Kang <kang@cgndev.com>
2021-10-31 03:08:25 +0900

    modify

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ class App {
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
 
-        document.body.appendChild(this);
+        document.body.appendChild(this.canvas);
 
         window.addEventListener('resize', this.resize.bind(this), false );
         this.resize();
