dda3235575754cb4680a205c43fbe255f23862f0
Byungchul Kang <kang@cgndev.com>
2021-11-05 00:44:15 +0900

    aa

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ class App {
     {
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
+        this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
 
         document.body.appendChild(this.canvas);
 
@@ -25,10 +26,10 @@ class App {
         this.stageWidth = document.body.clientWidth;
         this.stageHeight = document.body.clientHeight;
 
-        this.canvas.width = this.stageWidth * 2;
-        this.canvas.height = this.stageHeight * 2;
+        this.canvas.width = this.stageWidth * this.pixelRatio;
+        this.canvas.height = this.stageHeight * this.pixelRatio;
 
-        this.ctx.scale(2 , 2);
+        this.ctx.scale(this.pixelRatio , this.pixelRatio);
     }
 
     animate(t)
