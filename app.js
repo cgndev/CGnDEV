6690edd59b9849177018d2b4d61c8034533a58f1
Byungchul Kang <kang@cgndev.com>
2022-09-29 22:18:31 +0900

    loij

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
-const title = document.querySelector("div.hello:first-child h1");
-console.log(title);
-title.innerText = "Hello, Inside!";
-title.style.color = "blue";
+const h1 = document.querySelector("div.hello:first-child h1");
+console.log(h1);
+h1.innerText = "Hello, Inside!";
+h1.style.color = "blue";
 
 function handleTitleClick() {
-    title.innerText = "OK, Title clicked";
+    if(h1.style.color === "blue") {
+        h1.style.color = "tomato";
+    }
+    else {
+        h1.style.color = "blue";
+    }
+    h1.innerText = "OK, Title clicked";
 }
 
 function handleMouseEnter() {
-    title.innerText = "Mouse is here, click me";
+    h1.innerText = "Mouse is here, click me";
 }
 
 function handleMouseLeave() {
-    title.innerText = "Don't leave me";
+    h1.innerText = "Don't leave me";
 }
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "totato";
+}
+
+// function handleCopy() {
+//     window.alert("do not copy");
+// }
+
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+// title.onclick = handleTitleClick;
+// title.onmouseenter = handleMouseEnter;
+// title.onmouseleave = handleMouseLeave;
+
+// window.addEventListener("resize", handleWindowResize);
+// window.addEventListener("copy", handleCopy);
 
