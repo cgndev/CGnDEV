81a32f65b51bd243b2f101e5f145078ff67419c0
Byungchul Kang <kang@cgndev.com>
2023-01-09 13:20:22 +0900

    a

--- Backup/_BASIC_THREE_MAIN_LOCK_.js
+++ Backup/_BASIC_THREE_MAIN_LOCK_.js
@@ -4,20 +4,23 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
 
-const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+// 렌더러
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+// 카메라
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set( 0, 0, 5 );
+
+// 씬
+const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0xFF0000 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.set( 0, 0, 5 );
 
 function animate() {
     requestAnimationFrame( animate );
--- three/threeMain.js
+++ three/threeMain.js
@@ -3,30 +3,18 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
-
-const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0xFF4592 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const scene = new THREE.Scene();
 
-camera.position.set( 0, 0, 2 );
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
+camera.position.set( 16, 8, 50 );
+camera.lookAt( 0, 0, 0 );
 
-function animate() {
-    requestAnimationFrame( animate );
+renderer.render( scene, camera);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    renderer.render( scene, camera );
-
-};
-
-animate();
+window.onresize = function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+}
